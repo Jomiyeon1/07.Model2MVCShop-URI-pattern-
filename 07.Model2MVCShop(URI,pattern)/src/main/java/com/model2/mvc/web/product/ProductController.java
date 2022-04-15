f4d3ede70322501446369de72682c3c4998dce8b
bitcamp <bitcamp@DESKTOP-4PK2M08>
2022-04-15 17:06:31 +0900

    2022.04.15 17:06 /07 리팩토링 완료

--- 07.Model2MVCShop(URI,pattern)/src/main/java/com/model2/mvc/web/product/ProductController.java
+++ 07.Model2MVCShop(URI,pattern)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.model2.mvc.common.Page;
@@ -21,6 +22,7 @@ import com.model2.mvc.service.product.ProductService;
 
 //==> 상품관리 Controller
 @Controller
+@RequestMapping("/product/*")
 public class ProductController {
 
 	/// Field
@@ -37,91 +39,93 @@ public class ProductController {
 
 	@Value("#{commonProperties['pageSize']}")
 	int pageSize;
-	
-	
-	@RequestMapping("/addProductView.do")
-	public String addProductView() throws Exception{
-		
-		System.out.println("/addProdcutView.do");
-		
+
+	// @RequestMapping("/addProductView.do")
+	@RequestMapping(value = "addproduct", method = RequestMethod.GET)
+	public String addProduct() throws Exception {
+
+		System.out.println("/product/addProduct : GET");
+
 		return "redirect:/product/addView.jsp";
 	}
-	
-	@RequestMapping("/addProduct.do")
-	public String addProduct(@ModelAttribute("product") Product product) throws Exception{
-		
-		System.out.println("/addProduct.do");
-		//Business Logic
+
+	// @RequestMapping("/addProduct.do")
+	@RequestMapping(value = "addproduct", method = RequestMethod.POST)
+	public String addProduct(@ModelAttribute("product") Product product) throws Exception {
+
+		System.out.println("/product/addProduct : POST");
+		// Business Logic
 		productservice.addProduct(product);
-		
+
 		return "forward:/product/getProduct.jsp";
-		
-		
+
 	}
-	
-	// prodNo 못불러옴
-	@RequestMapping("/getProduct.do")
-	public String getProduct(@RequestParam("prodNo") int prodNo, Model model) throws Exception{
-		
-		System.out.println("/getProduct.do");
-		
+
+	// @RequestMapping("/getProduct.do")
+	@RequestMapping(value = "getProduct", method = RequestMethod.GET)
+	public String getProduct(@RequestParam("prodNo") int prodNo, Model model) throws Exception {
+
+		System.out.println("/product/getProduct : GET ");
+
 		Product product = productservice.getProduct(prodNo);
-		
+
 		model.addAttribute("product", product);
-		
+
 		return "forward:/product/getProduct.jsp";
-		
+
 	}
-	
-	@RequestMapping("/updateProductView.do")
-	public String updateProductView(@RequestParam("prodNo") int prodNo, Model model) throws Exception{
+
+	//@RequestMapping("/updateProductView.do")
+	//public String updateProductView(@RequestParam("prodNo") int prodNo, Model model) throws Exception{
+	@RequestMapping(value = "updateProduct", method = RequestMethod.GET)
+	public String updateProduct(@RequestParam("prodNo") int prodNo, Model model) throws Exception {
 		
-		System.out.println("/updateProductView.do");
-		System.out.println("update"+prodNo);
+		System.out.println("/product/updateProduct : GET");
+		System.out.println("update" + prodNo);
 		Product product = productservice.getProduct(prodNo);
-		
+
 		model.addAttribute("product", product);
-		
+
 		return "forward:/product/updateProduct.jsp";
 	}
+
 	
-	/// 수정중
-	@RequestMapping("/updateProduct.do")
-	public String updateProduct(@ModelAttribute("product") Product product, Model model) throws Exception{
-		
-		System.out.println("/updateProduct.do");
-		
+	//@RequestMapping("/updateProduct.do")
+	//public String updateProduct(@ModelAttribute("product") Product product, Model model) throws Exception {
+	@RequestMapping(value = "updateProduct", method = RequestMethod.POST)
+	public String updateProduct(@ModelAttribute("product") Product product, Model model) throws Exception {
+		System.out.println("/product/updateProduct : POST");
+
 		productservice.updateProduct(product);
-		
-		return "redirect:/getProduct.do?prodNo="+product.getProdNo();
+
+		return "forward:/product/getProduct?prodNo=" + product.getProdNo();
 	}
-	
-	
+
 	/////////////
-	@RequestMapping("/listProduct.do")
-	public String listProduct(@ModelAttribute("search") Search search, Model model, HttpServletRequest request,@RequestParam("menu") String menu) throws Exception{
-		
-		System.out.println("/listProduct.do");
-		
-		if(search.getCurrentPage() ==0 ){
+	//@RequestMapping("/listProduct.do")
+	@RequestMapping(value = "listProduct")
+	public String listProduct(@ModelAttribute("search") Search search, Model model, HttpServletRequest request,
+			@RequestParam("menu") String menu) throws Exception {
+
+		System.out.println("/product/listProduct : GET / POST");
+
+		if (search.getCurrentPage() == 0) {
 			search.setCurrentPage(1);
 		}
 		search.setPageSize(pageSize);
-		
-		Map<String , Object> map = productservice.getProductList(search);
-		
-		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+
+		Map<String, Object> map = productservice.getProductList(search);
+
+		Page resultPage = new Page(search.getCurrentPage(), ((Integer) map.get("totalCount")).intValue(), pageUnit,
+				pageSize);
 		System.out.println(resultPage);
-		
-		
+
 		model.addAttribute("list", map.get("list"));
 		model.addAttribute("resultPage", resultPage);
 		model.addAttribute("search", search);
 		model.addAttribute("menu", menu);
-		
+
 		return "forward:/product/listProduct.jsp";
 	}
-		
-	
-	
+
 }
